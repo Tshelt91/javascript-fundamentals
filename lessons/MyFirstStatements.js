8f6937aa420b437fba201eaac2099272cc31433a
Teauizje' Shelton <teauizjeshelton@gmail.com>
2024-04-01 14:04:36 -0500

    Update1

---
+++ lessons/MyFirstStatements.js
@@ -0,0 +1,9 @@
+console.log("Hello world")
+
+console.log("This is my first sentence")
+
+console.log("I am new to JavaScript")
+
+console.log("I am learning stuff every day")
+
+console.log("Goodbye")
