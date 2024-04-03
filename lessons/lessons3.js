239f05ac5184f6501f226c6d71d37066f140c677
Tshelt91 <teauizjeshelton@gmail.com>
2024-04-03 12:56:01 -0500

    Update2

---
+++ lessons/lessons3.js
@@ -0,0 +1,75 @@
+// Declare a string using single quotes
+let felineType = 'Cat'
+let felineAction = 'They meow quietly';
+
+// Declare a string using doubble quotes
+let canineType = "Dog";
+let canineAction = "They bark loudly."
+
+/*Using bracket notation on a variable of string
+will output 1 characters, or undefined */
+console.log(canineType[3])
+console.log(felineAction[7])
+console.log(canineAction[4]);
+console.log(felineType[0]);
+
+let longSentence =
+"Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet," + 
+"consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor" +
+"sit amet, consectetur Lorem ipsum dolor sit amet, consectetur";
+
+let longSecond = "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit ametconsectetur.";
+
+
+const alpha = 'Hello';
+const beta = " World";
+
+const combinedVariables = alpha + beta;
+console.log(combinedVariables);
+
+const combined2 = alpha + 40;
+console.log(combined2);
+
+const combined3 = "The" + beta + " is vast.";
+console.log(combined3);
+
+const VALUE1 = 10;
+const VALUE2 = 20;
+
+console.log(
+    'Thirty is ' + (VALUE1 + VALUE2) + ' and not ' + (2 * VALUE1 + VALUE2));
+
+
+    console.log(`Thirty is ${VALUE1 + VALUE2} and not ${2 * VALUE1 + VALUE2}`);
+    console.log(`Here are more ${combinedVariables} , ${combined2}`);
+    console.log(`Canines are ${canineType}. ${canineAction}`);
+
+    //Single character form a UTF-8 caracter set
+    let letter = '\u00A9'
+    console.log(letter);
+
+    let phrase = 'My name Roger 0\'Dell. \tHow are \n you doing today';
+    console.log(phrase);
+
+    let phrase2 = 'Let\"s try   \\something \0';
+    console.log(phrase2);
+
+    //Using the string length proprty
+    const example_length = "Lorem ipsum dolor sit amet,";
+    console.log(example_length.length);
+
+    const msg = ' Hello, world ';
+
+    console.log(msg.at(-2));
+    console.log(msg.at(4));
+
+    console.log(msg.charAt(2));
+
+    console.log(msg.includes('He'));
+    console.log(msg.indexOf('w'));
+    console.log(msg.toUpperCase());
+
+
+]
+
+
