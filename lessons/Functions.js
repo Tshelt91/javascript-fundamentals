238ae65db4e6cc7667344e629b608aaa7bc7ae74
Tshelt91 <teauizjeshelton@gmail.com>
2024-04-08 12:57:39 -0500

    NewUpdates

---
+++ lessons/Functions.js
@@ -0,0 +1,28 @@
+//Task One
+function homework (){
+    const intro = "Hello, my name is";
+    const name = "Teauizje";
+    console.log(intro + name);    
+};
+
+function homeworkTwo() {
+    const name = "Teauizje"
+    const age = 32
+    console.log(`Hello, my name is ${name} and I am ${32} yaers old.`)
+}
+
+//Task Two
+function homeworkThree(value1, value2 = 15) {
+    let value1 = 10;
+    let value2 = 5;
+    return value1 + value2;
+    console.log(value1 + value2);
+    console.log(value2 + value1);
+};
+
+//Task Three
+const homeworkFour = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(example(1, 2));
+
