b2e8fa21edc7c92e55b69a175ac7a4713603bdb2
Tshelt91 <teauizjeshelton@gmail.com>
2024-04-10 12:46:24 -0500

    Looping

---
+++ looping.js
@@ -0,0 +1,20 @@
+//Task One
+function whileHomework() {
+    let age = 5;
+    while (age < 26) {
+      console.log(age);
+      age++;
+    }
+  }
+ whileHomework();
+
+//Task Two
+
+const oddNumbers = () => {
+  for (num3 = 0; num3 <= 30; num3++) {
+    if (num3 % 3 == 0) {
+      console.log(`${num3} is odd.`);
+    }
+  }
+};
+oddNumbers();
