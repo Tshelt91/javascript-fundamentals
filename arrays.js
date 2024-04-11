2ad4092c490b925dde846c8c32d45ec626a9fd76
Tshelt91 <teauizjeshelton@gmail.com>
2024-04-11 14:46:13 -0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,6 @@
+const icecreamFlavors = ["Chocolate", "Strawberry", "Vanilla", "Mint", "birthdayCake", "butterPecan", "blackWalnut", "cookieDough", "mooseTracks", "Neopolitan"];
+console.log(icecreamFlavors);
+
+const joinFlavors = icecreamFlavors.join(' & ');
+console.log(joinFlavors);
+ 
